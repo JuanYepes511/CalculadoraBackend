30b0066b0015b44ceee404f5c62af086d1d4b06f
JuanCamiloYepesAlbarracin <jyepes511@unab.edu.co>
2023-08-22 06:32:01 -0500

    Backend001-feat Desarrollar calculadora y conversor

---
+++ Calculadora/Calculadora.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora PHP</title>
+</head>
+<body>
+    <h2>Calculadora PHP</h2>
+    <form action="calculadora.php" method="post">
+        <label for="num1">Número 1:</label>
+        <input type="number" name="num1" required><br>
+        
+        <label for="num2">Número 2:</label>
+        <input type="number" name="num2" required><br>
+        
+        <label for="operation">Operación:</label>
+        <select name="operation">
+            <option value="sum">Suma</option>
+            <option value="subtract">Resta</option>
+            <option value="multiply">Multiplicación</option>
+            <option value="divide">División</option>
+        </select><br>
+        
+        <input type="submit" value="Calcular">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $operation = $_POST["operation"];
+        
+        switch ($operation) {
+            case "sum":
+                $result = $num1 + $num2;
+                break;
+            case "subtract":
+                $result = $num1 - $num2;
+                break;
+            case "multiply":
+                $result = $num1 * $num2;
+                break;
+            case "divide":
+                if ($num2 != 0) {
+                    $result = $num1 / $num2;
+                } else {
+                    $result = "No se puede dividir por cero";
+                }
+                break;
+            default:
+                $result = "Operación no válida";
+                break;
+        }
+        
+        echo "<p>Resultado: $result</p>";
+    }
+    ?>
+</body>
+</html>
---
+++ Calculadora/Conversor.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Conversor de Moneda</title>
+</head>
+<body>
+    <h1>Conversor de Moneda</h1>
+    <form action="conversor.php" method="post">
+        <label for="monto">Monto en Pesos Colombianos:</label>
+        <input type="number" name="monto" id="monto" required>
+        <label for="moneda">Seleccione la moneda a la que desea convertir:</label>
+        <select name="moneda" id="moneda">
+            <option value="usd">Dólares (USD)</option>
+            <option value="eur">Euros (EUR)</option>
+            <option value="inr">Rupia India (INR)</option>
+        </select>
+        <input type="submit" value="Convertir">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $monto = $_POST["monto"];
+        $moneda_destino = $_POST["moneda"];
+        
+        // Tasa de conversión aproximada (a modo de ejemplo)
+        $tasa_usd = 0.00024;
+        $tasa_eur = 0.00022;
+        $tasa_inr = 0.020;
+        
+        $conversion = 0;
+        $moneda_nombre = "";
+        
+        switch ($moneda_destino) {
+            case "usd":
+                $conversion = $monto * $tasa_usd;
+                $moneda_nombre = "Dólares (USD)";
+                break;
+            case "eur":
+                $conversion = $monto * $tasa_eur;
+                $moneda_nombre = "Euros (EUR)";
+                break;
+            case "inr":
+                $conversion = $monto * $tasa_inr;
+                $moneda_nombre = "Rupia India (INR)";
+                break;
+        }
+        
+        echo "<p>$monto Pesos Colombianos equivalen a $conversion $moneda_nombre.</p>";
+    }
+    ?>
+</body>
+</html>
